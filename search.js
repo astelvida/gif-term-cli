fe112a3eae00bb41bf76c176fa1ee2792bd0a706
astelvida <sevda.m.anefi@gmail.com>
2019-10-15 07:20:40 +0100

    add temporary cursor position files to fix deprecation on node >12

--- search.js
+++ search.js
@@ -3,105 +3,109 @@ const readline = require('readline');
 const chalk = require('chalk');
 const clipboardy = require('clipboardy');
 const t = require('ansi-escapes');
-const getCursorPosition = require('get-cursor-position');
+const getCursorPosition = require('./cursor-position.js');
+
 const ora = require('ora');
 
-async function search (text, flags) {
+
+async function search(text, flags) {
     let output;
     let url;
     let error;
-    let pos;
-    let isRendered;
+    let currentPosition;
     let isFetching = false;
     let hasStarted = false;
-    let spinner;
     text = text || '';
+    let spinner = ora(text)
+    let copiedMsg = chalk`{hex('#1DE9B6') ✔} {hex('#B388FF') gif copied to clipboard}`
+
+
+    function handleExit(clip = false) {
+        let message = ''
+        if (error) {
+            message = error
+        } else if (clip) {
+            clipboardy.writeSync(url);
+            message = copiedMsg
+        } 
+        currentPosition = currentPosition || getCursorPosition.sync()
+        readline.clearLine(process.stdout, 0);
+        readline.cursorTo(process.stdout, 0, currentPosition.row)
+
+        process.stdout.write(message + '\n');
+        process.exit(0);
+    }
+    async function fetchAndSetGif() {
+        const data = await gifTerm.data(text, flags) || {};
+        output = data.imgStr
+        url = data.url || '';
+        error = data.errorMsg || '';
+    }
 
     process.stdout.write(t.cursorHide);
 
-    function handleExit(clip = false, message = '') {
-        if (clip && !error && url && isRendered) {
-            clipboardy.writeSync(url);
-            message = chalk`{hex('#1DE9B6') ✔} {hex('#B388FF') gif copied to clipboard}\n`;
-        }
-
-        let row = (!pos || !pos.row) ? getCursorPosition.sync().row : pos.row;
-        readline.cursorTo(process.stdout, 0, row - 1);
-        readline.clearLine(process.stdout, 0);
-        process.stdout.write(message + '\r\n');
-        process.exit(0);
-    }
-
-    async function fetchAndSetGif() {
-        spinner = ora(text).start();
-        const data = await gifTerm.data(text, flags) || {};
-        output = data.imgStr || data.errorMsg;
-        url = data.url || '';
-        error = !!data.errorMsg;
-    }
-
     if (text) {
+        spinner.start()
         await fetchAndSetGif();
         spinner.clear();
-        console.log('\n' + output);
-        pos = getCursorPosition.sync();
-        isRendered = true;
-        process.stdout.write(t.cursorRestorePosition);
+        process.stdout.write(output + '\n');
+        currentPosition = getCursorPosition.sync();
         return handleExit(flags.clip);
     }
-    
+
     readline.emitKeypressEvents(process.stdin);
     process.stdin.setRawMode(true);
 
     const introMsg = 'start typing and hit return to get matching GIF';
     const prompt = chalk`{hex('#1DE9B6') ❯}`;
-    process.stdout.write('\n');
-    process.stdout.write(t.cursorSavePosition);
     process.stdout.write(prompt + ' ');
     process.stdout.write(chalk`{bold.dim ${introMsg}}`);
 
+    
+
     process.stdin.on('keypress', async (str, key) => {
-        if (isFetching) {
+
+        if (isFetching || !str) {
             return;
-        }
+        } 
+
         if (key.name === 'return') {
-            if (!text) return;
             
-            process.stdout.write('\r');
-            isFetching = true;
+            isFetching = true
+            spinner.start();
+            
             await fetchAndSetGif();
 
+            spinner.stopAndPersist({ symbol: prompt, text });
+            process.stdout.write(t.cursorHide);
+
             readline.clearScreenDown(process.stdout);
-            console.log('\n' + output);
-            pos = getCursorPosition.sync();
-            isRendered = true;
-            process.stdout.write(t.cursorRestorePosition);
-            isFetching = false;
-            spinner.stopAndPersist({ symbol: prompt });
-            readline.moveCursor(process.stdout, text.length + 2, -1);
-            return;
-         
-        } else if (key.name === 'escape' || (key.ctrl && key.name === 'c')) {
-            if (error || !isRendered) return handleExit(false);
-
-            if (flags.clip) return handleExit(true);       
+            process.stdout.write(output + '\n');
             
-            let row = (!pos || !pos.row) ? getCursorPosition.sync().row : pos.row;
-            readline.cursorTo(process.stdout, 0, row - 1);
+            currentPosition = getCursorPosition.sync();
+            readline.cursorTo(process.stdout, 0, currentPosition.row)
+            process.stdout.write(prompt + ' ' + text);
+            process.stdout.write(t.cursorShow);
+            
+            isFetching = false
+            return;
 
-            process.stdout.write(chalk`{hex('#1DE9B6').bold ? } {bold.dim Save to clipboard? {white (y/N)}} `);
+        } else if (key.name === 'escape' || (key.ctrl && key.name === 'c')) {
+            currentPosition = currentPosition || getCursorPosition.sync()
+            readline.cursorTo(process.stdout, 0, currentPosition.row);
+            process.stdout.write(chalk`{hex('#1DE9B6').bold ? } {bold.dim Save to clipboard? {white (y/N)}}`);
+
             process.stdin.on('keypress', (char, key) => {
-                let clip = (char && char.match(/(y|yes|yeah|yep)/gi)) || false; 
+                let clip = (char && char.match(/(y|yes|yeah|yep)/gi));
                 return handleExit(clip);
             });
 
         } else if (key.name === 'backspace') {
-            if (!text) return;
-
+            if (!text) return
             text = text.slice(0, text.length - 1);
             readline.moveCursor(process.stdout, -1, 0)
             readline.clearLine(process.stdout, 1);
-    
+
         } else if (str && key.ctrl === false) {
             if (!hasStarted) {
                 readline.moveCursor(process.stdout, - introMsg.length, 0);
@@ -113,6 +117,11 @@ async function search (text, flags) {
         }
     })
 
+    process.stdin.on('error',
+        (err) => {
+            error = err.message
+            handleExit(false)
+        })
 }
 
 module.exports = { search };
